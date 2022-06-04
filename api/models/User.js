58933d190c4647522b65e6a6b4080292df86b16f
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-04 23:46:42 +0700

    Create backend login and register

--- api/models/User.js
+++ api/models/User.js
@@ -20,6 +20,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    desc: {
+      type: String,
+      required: false,
+    },
   },
   { timestamps: true }
 );
