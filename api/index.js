b6ea245094f5f4750d72859a7dc20afffcc18851
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-04 21:15:23 +0700

    Connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
 
 app.use("/", (req,res)=>{
     console.log("This is main url")
