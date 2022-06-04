98ede566b640bc1a509f532a998b0a61e512909f
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-05 02:47:44 +0700

    Create post CRUD and category CR

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(port, () =>{
     console.log("Backend is running at port " + port + ".")
